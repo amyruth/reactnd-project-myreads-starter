f493aaefd3209b1f485b216e2024adcbc26c916d
amyruth <anrutherford@gmail.com>
2018-09-08 14:18:18 -0400

    starting this shelf thing over again

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React from 'react';
-import * as BooksAPI from './BooksAPI';
-import './App.css';
-import Header from './Header';
-import Book from './Book';
-import SearchPage from './SearchPage';
 import MainPage from './MainPage';
-import Shelf from './Shelf';
-import OpenSearchBtn from './OpenSearchBtn';
+import './App.css';
+import * as BooksAPI from './BooksAPI';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-		showSearchPage: false,
-		books: []
-  }
-
-  componentDidMount() {
-	  BooksAPI.getAll()
-	  .then((booksfromserver) => {
-		  this.setState({books: booksfromserver});
-		  console.log(this.state.books);
-	  })	
+	state = {
+		/**
+		 * TODO: Instead of using this state variable to keep track of which page
+		 * we're on, use the URL in the browser's address bar. This will ensure that
+		 * users can use the browser's back and forward buttons to navigate between
+		 * pages, as well as provide a good URL they can bookmark and share.
+		 */
+			showSearchPage: false,
+			books: []
+	  }
+	
+	componentDidMount() {
+		BooksAPI.getAll()
+		.then((books) => {
+			this.setState({ books: books });
+			console.log(this.state.books);
+		})	
 	}
 
 	render() {
 
 		return (
 			<div className="app">
-				<MainPage />
+				<MainPage books={this.state.books} />
   			</div> //end app
 		);
 	}
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
-import Book from './Book';
 import Header from './Header';
-import Shelf from './Shelf';
+import Book from './Book';
 import OpenSearchBtn from './OpenSearchBtn';
+import SearchPage from './SearchPage';
+
 
 class MainPage extends Component {
 	render() {
+	
 		return (
 			<div className="list-books">
 				<Header />
-
 				<div className="list-books-content">
 
 					<div className="shelf-holder">
+							<div className="bookshelf">
+								<h2 className="bookshelf-title">Currently Reading</h2>
+								<div className="bookshelf-books">
+									<ol className="books-grid">	
+										this.props.books.filter((book) => {
+											if (book.shelf === 'currentlyReading') {
+												<Book />
+											}
+										})
+									</ol>
+								</div>
+							</div>
 
-						<Shelf />
+						<div className="bookshelf">
+							<h2 className="bookshelf-title">Want to Read</h2>
+							<div className="bookshelf-books">
+								<ol className="books-grid">	
 
-						<Shelf />
+								</ol>
+							</div>
+						</div>
 
-						<Shelf />
+						<div className="bookshelf">
+							<h2 className="bookshelf-title">Read</h2>
+							<div className="bookshelf-books">
+								<ol className="books-grid">		
+													
+								</ol>
+							</div>
+						</div>
 					</div> { /*end shelf-holderr*/ }
 				
 				</div> { /*end list-books-content */}
